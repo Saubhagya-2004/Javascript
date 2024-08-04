2914df59960fb486fd5f2ef1600be1ab3612f927
Saubhagya Baliarsingh <saubhagyabaliarsingh2@gmail.com>
2024-08-04 12:45:41 +0530

    changes

---
+++ Script/datatype02.js
@@ -0,0 +1,17 @@
+"user strict";//treat all js code as newer version
+console.log("Chiku")
+console.log(8+8)
+let name="devil"
+let age=20
+let isloggedin=false
+//number= 2 to power 53
+//bigint=if our number is very big then its use :It's basically used in trading
+//String=>""
+//boolean=true/false
+//null=it is used to absent of any object(standalone value)
+//undefine=>if we are not assign a value
+let city
+//symbol=define uniqueness
+console.log(typeof"chiku")
+//define type
+console.table([name,age])
