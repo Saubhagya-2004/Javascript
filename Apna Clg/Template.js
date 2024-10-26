db2e3f09ce1e7b2ee67f6e904da194c4ee7a077c
Saubhagya Baliarsingh <saubhagyabaliarsingh2@gmail.com>
2024-10-26 23:17:58 +0530

    changes

---
+++ Apna Clg/Template.js
@@ -0,0 +1,6 @@
+const pen=23;
+const pencil=36;
+// let output="Total price is "+( pen+pencil)+"Rupees";
+// ``back tick is used to add string
+let output= `Total price is ${pen+pencil}`;
+console.log(output);
