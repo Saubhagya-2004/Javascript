b24d76a37e5e53b224d617eacfcf459d99742875
Saubhagya Baliarsingh <saubhagyabaliarsingh2@gmail.com>
2024-10-27 21:24:47 +0530

    changes

---
+++ Apna Clg/8-Switch.js
@@ -0,0 +1,14 @@
+let color ='Green';
+switch(color){
+    case 'Red':
+        console.log('Stop Here');
+        break;
+        case 'Yellow':
+            console.log('Wait');
+            break;
+            case 'Green':
+                console.log('Go');
+                break;
+                default:
+                    console.log('Light is Broken');
+}
