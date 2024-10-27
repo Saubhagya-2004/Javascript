3484c66f800765c2e22d03d37db15e888913caf6
Saubhagya Baliarsingh <saubhagyabaliarsingh2@gmail.com>
2024-10-27 20:28:20 +0530

    changes

---
+++ Apna Clg/7-Truthy-false.js
@@ -0,0 +1,15 @@
+let num =0;
+if(num){
+    console.log('Num is falsy');
+}
+else{
+    console.log('Num is Truthy');
+}
+let str=null
+if(str){
+    console.log('Hi');
+}
+else{
+    console.log('Bye');
+}
+// Falsy value-->false,nan,null,undefined,'';
