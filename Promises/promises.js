1e4d76f30640f3ebac3943a787f4970cce299e49
Saubhagya Baliarsingh <saubhagyabaliarsingh2@gmail.com>
2024-12-16 14:54:28 +0530

    changes

---
+++ Promises/promises.js
@@ -0,0 +1,62 @@
+const promises=new Promise(function(resolve,reject){
+    //do an async task
+    // db clls,cryptography
+    setTimeout(function(){
+        console.log('Async task is completed');
+        resolve();
+        
+    },1000);
+})
+promises.then(function(){
+    console.log('promise consumed');
+    
+})
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log('Async task2');
+        resolve();
+        
+    },1000);
+}).then(function(){
+    console.log('Async resolved');
+    
+})
+//2
+const promise2=new Promise(function(resolve,reject){
+    setTimeout(function(){
+
+        resolve({user:'saubhagya',email:'bank.gmail'});
+    },1000)
+});
+promise2.then(function(result){
+    //here result is user
+    console.log(result);
+    
+})
+//3
+const promise3=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        // let error=true; //if error true then return else
+        let error=false;
+        if(!error){
+            resolve({username:'chiku',Email:'chiku@gmail. com'})
+        }
+        else{
+            reject('error something went wrong')
+        }
+    },1000)
+});
+promise3.then(function(user){
+    console.log(user);
+    return user.username;//access only username
+    //heres username acess .then under
+    
+}).then((username)=>{
+    console.log(username);
+    //this is called channing
+    //use .then multiple time  acess upper value here
+
+}).catch(function(errr){
+console.log(errr);
+
+})
