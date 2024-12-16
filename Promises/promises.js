3ef92c0be2d0d0fb3f1dfca576b06407596f9c7f
Saubhagya Baliarsingh <saubhagyabaliarsingh2@gmail.com>
2024-12-16 21:08:41 +0530

    changes

--- Promises/promises.js
+++ Promises/promises.js
@@ -51,8 +51,8 @@ promise3.then(function(user){
     return user.username;//access only username
     //heres username acess .then under
     
-}).then((username)=>{
-    console.log(username);
+}).then((usernae)=>{
+    console.log(usernae);
     //this is called channing
     //use .then multiple time  acess upper value here
 
@@ -60,3 +60,41 @@ promise3.then(function(user){
 console.log(errr);
 
 })
+//4
+//async function use
+const promise4=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        // let error=true; //if error true then return else
+        let error=true;
+        if(!error){
+            resolve({username:'chiku',Email:'chiku@gmail. com'})
+        }
+        else{
+            reject('error something went wrong')
+        }
+    },1000)
+});
+async function consumed() {
+    // if await keyword use without try cath it runs if error not occurs whenever it occurs await keyword give error
+    try{
+        const response=await promise4
+    console.log(response);
+    }
+    catch(error){
+console.log(error);
+
+    }
+}
+consumed();
+async function getalluser(params) {
+    try {
+        const response=await fetch('https://jsonplaceholder.typicode.con/user');
+        const data=await response.json();
+        console.log(data);
+        
+    } catch (error) {
+        console.log(error);
+        
+    }  
+}
+getalluser();
