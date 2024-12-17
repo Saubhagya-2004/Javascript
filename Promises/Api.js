43ac29d86a726a76f9b2720d09163a37fcc52559
Saubhagya Baliarsingh <saubhagyabaliarsingh2@gmail.com>
2024-12-18 00:31:19 +0530

    changes

--- Promises/Api.js
+++ Promises/Api.js
@@ -2,10 +2,11 @@ let json= {
     "fact": "Ailurophile is the word cat lovers are officially called.",
     "length": 57
 };
+// https://dog.ceo/api/breeds/image/random  valid api
 console.log(json);
 // let val=JSON.parse(json);
 // console.log(val);
 let student={name:'saubhagya',age:'23',Qualification:'Btech cse'};
 console.log(JSON.stringify(student));
-//convert it into valid api
+//convert it into valid json data
 
