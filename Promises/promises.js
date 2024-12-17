9276c2a300521e854c5075ab5b9c02a0f156fa71
Saubhagya Baliarsingh <saubhagyabaliarsingh2@gmail.com>
2024-12-17 10:48:13 +0530

    changes

--- Promises/promises.js
+++ Promises/promises.js
@@ -86,15 +86,15 @@ console.log(error);
     }
 }
 consumed();
-async function getalluser(params) {
-    try {
-        const response=await fetch('https://jsonplaceholder.typicode.con/user');
-        const data=await response.json();
-        console.log(data);
+// async function getalluser(params) {
+//     try {
+//         const response=await fetch('https://jsonplaceholder.typicode.con/user');
+//         const data=await response.json();
+//         console.log(data);
         
-    } catch (error) {
-        console.log(error);
+//     } catch (error) {
+//         console.log(error);
         
-    }  
-}
-getalluser();
+//     }  
+// }
+// getalluser();
---
+++ Promises/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+async function getalluser(params) {
+//     try {
+//         const response=await fetch('https://jsonplaceholder.typicode.con/user');
+//         const data=await response.json();
+//         console.log(data);
+        
+//     } catch (error) {
+//         console.log(error);
+        
+//     }  
+// }
+// getall
